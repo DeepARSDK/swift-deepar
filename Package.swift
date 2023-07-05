9fe1266ee4887a1ffb8acc3fb336f28582ad8069
Hrvoje Ditrih <hrvoje.ditrih@gmail.com>
2023-07-05 15:23:36 +0200

    5.3.0-alpha-893

--- Package.swift
+++ Package.swift
@@ -1,12 +1,10 @@
 // swift-tools-version: 5.8
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "DeepAR",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DeepAR",
             targets: ["DeepAR"]),
@@ -14,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DeepAR",
-            url: "https://s3.eu-west-1.amazonaws.com/sdk.developer.deepar.ai/ci/DeepAR-iOS-v5.3.0-alpha-1.zip",
-            checksum: "2c91954d588ca857884c23c7af8380a185517eecb951869d38df98641428b225"
+            url: "https://s3.eu-west-1.amazonaws.com/sdk.developer.deepar.ai/ci/ios-distribution/DeepAR-iOS-v5.3.0-alpha-893.zip",
+            checksum: ""
         )
     ]
 )
