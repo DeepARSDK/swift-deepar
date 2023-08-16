be53632b907eb71034aaddae4943c3b2e8d87666
Darko Janekovic <darko@deepar.ai>
2023-08-16 16:50:57 +0200

    5.4.1-revert-lazy-load-alpha-35

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DeepAR",
-            url: "https://s3.eu-west-1.amazonaws.com/sdk.developer.deepar.ai/ci/ios-distribution/DeepAR-iOS-v5.4.0.zip",
-            checksum: "f5cb8e18fadcb277179ff65ca97061c8abc430d9038cfe442dcc9c483777aff4"
+            url: "https://s3.eu-west-1.amazonaws.com/sdk.developer.deepar.ai/ci/ios-distribution/DeepAR-iOS-v5.4.1-revert-lazy-load-alpha-35.zip",
+            checksum: "3c656ec392e5addea1c5ddef6ea358b655a6afb25c8fe2f82eec8bc9edaeb345"
         )
     ]
 )
