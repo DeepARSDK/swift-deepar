dc25c32349b4984264f0aaf337a0a0940be9823b
Darko Janekovic <darko@deepar.ai>
2023-09-12 15:46:41 +0200

    5.4.2-shutdown-bugfix-alpha-45

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DeepAR",
-            url: "https://s3.eu-west-1.amazonaws.com/sdk.developer.deepar.ai/ci/ios-distribution/DeepAR-iOS-v5.4.1.zip",
-            checksum: "ce2f6ab209ed151b6cbc5b33b6875b35d64e8830bd2fa2ffaa257cb2247cc0f6"
+            url: "https://s3.eu-west-1.amazonaws.com/sdk.developer.deepar.ai/ci/ios-distribution/DeepAR-iOS-v5.4.2-shutdown-bugfix-alpha-45.zip",
+            checksum: "c241e47c2eab1191064e5b38f5b987f3228dcb4829b032933757c4e2d16444db"
         )
     ]
 )
