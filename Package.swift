a3a537b15dfb19bceb46d68c42be00860d8e5981
Darko Janekovic <darko@deepar.ai>
2023-09-14 18:18:07 +0200

    5.4.2-alpha-55

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DeepAR",
-            url: "https://s3.eu-west-1.amazonaws.com/sdk.developer.deepar.ai/ci/ios-distribution/DeepAR-iOS-v5.4.2-alpha-51.zip",
-            checksum: "716b268bfe5cb9de6a55546f0643c49d6844b46b675b8443976e05e59f438fc2"
+            url: "https://s3.eu-west-1.amazonaws.com/sdk.developer.deepar.ai/ci/ios-distribution/DeepAR-iOS-v5.4.2-alpha-55.zip",
+            checksum: "ecb1f25039a77ac04f20ac136c05a8bf07b27a8cd1922a50d47669aae7d3c238"
         )
     ]
 )
