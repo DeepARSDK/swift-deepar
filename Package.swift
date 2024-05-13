1bf3667519367387cb4f4324b4d78015561bc001
Hrvoje Ditrih <hrvoje.ditrih@gmail.com>
2024-05-13 10:49:45 +0200

    5.6.3

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DeepAR",
-            url: "https://s3.eu-west-1.amazonaws.com/sdk.developer.deepar.ai/ci/ios-distribution/DeepAR-iOS-v5.6.2.zip",
-            checksum: "43dd5b4cf7852613d5ee0d87997fb5fd5fc15ff37d2c2ea6883013c09b403aca"
+            url: "https://s3.eu-west-1.amazonaws.com/sdk.developer.deepar.ai/ci/ios-distribution/DeepAR-iOS-v5.6.3.zip",
+            checksum: "ec90218ba4af8820c6fa7d09a05636dc91b6d563c61cf70ba4684a73a4304a6b"
         )
     ]
 )
